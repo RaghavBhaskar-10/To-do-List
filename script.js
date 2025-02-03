e4d1c046b7380e40a5e49c95ae2273ece2947d4b
Raghav Bhaskar <rbhaskar0707@gmail.com>
2025-02-03 10:19:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+const inputBox = document.getElementById("input-box");
+const listContainer =  document.getElementById("list-container");
+const deleteButton = document.getElementById("delete");
+function addTask(){
+    if( inputBox.value === ''){
+        alert("You must write something");
+    }
+    else{
+        let li = document.createElement("li")
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        var ckeckedItem =  e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+},false)
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask(); 
+
+inputBox.addEventListener("keypress", function(e){
+    if(e.key === "Enter"){
+        addTask();
+    }
+})
+
+// function deleteTask(){
+//   listContainer.innerHTML = "";
+// }
+
+deleteButton.addEventListener("click", function(){
+    ckeckedItem.remove()
+})
